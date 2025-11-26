577274ff94b7424348fa6c10f77e5c75c34a533a
EstebanRomeo <estebannicolasromeo@gmail.com>
2025-11-26 20:12:32 -0300

    Cambios 26/10

--- src/Componentes/Servicios/Servicios.js
+++ src/Componentes/Servicios/Servicios.js
@@ -6,7 +6,7 @@ const Servicios = () => {
   return (
     <div className='containerServicios'>
         <div className='containerTitleServ'>
-            <h2>Servicios</h2>
+            <h2>Roles que desempeño</h2>
         </div>
         <div className='servicios'>
 
@@ -15,14 +15,19 @@ const Servicios = () => {
                 <div class="brutalist-card__icon">
 
                 </div>
-                <div class="brutalist-card__alert">Web Design</div>
+                <div class="brutalist-card__alert">DISEÑADOR</div>
             </div>
             <div class="brutalist-card__message">
+                <p>
+                    Valoro las interfaces limpias, la estructura clara y las experiencias que se sienten naturales para el usuario.
+                </p>
             <ul>
-                    <li>Creación de interfaces modernas, responsivas y optimizadas</li>
-                    <li>Uso de tecnologías como HTML, CSS, JavaScript, React.js y TailwindCSS .</li>
-                    <li>Enfoque en experiencia de usuario (UX) y diseño atractivo (UI).</li>
+                    <li>UX, UI</li>
+                    <li>Aplicaciones Web</li>
+                    <li>interfaces responsivas</li>
                 </ul>
+                <p>Herramientas:
+Figma, Canva, lápiz y papel, goconqr prototipado rápido.</p>
             </div>
             </div>
 
@@ -34,14 +39,20 @@ const Servicios = () => {
                 <div class="brutalist-card__icon">
 
                 </div>
-                <div class="brutalist-card__alert">Web FullStack</div>
+                <div class="brutalist-card__alert">FULLSTACK DEVELOPER</div>
             </div>
             <div class="brutalist-card__message">
+                <p>
+                    Disfruto transformar ideas en productos reales, desde el diseño hasta el desarrollo de sistemas .
+                </p>
             <ul>
                     <li>Integración de frontend con backend usando Node.js, Express y SQL.</li>
                     <li>Desarrollo de APIs eficientes y seguras.</li>
                     <li>Creación de sistemas de autenticación y gestión de usuarios.</li>
                 </ul>
+                <p>
+                    Herramientas de desarrollo: Git, GitHub, VS Code, TailwindCSS(en proceso).
+                </p>
             </div>
             </div>
 
@@ -53,13 +64,24 @@ const Servicios = () => {
                 <div class="brutalist-card__icon">
 
                 </div>
-                <div class="brutalist-card__alert">Software a medida</div>
+                <div class="brutalist-card__alert">LIDER DE CODELAUNCH</div>
             </div>
             <div class="brutalist-card__message">
+                <p>
+                    Soy fundador y líder de CodeLaunch, una startup que conecta programadores de todo el mundo con proyectos reales para que ganen experiencia, mejoren sus habilidades y trabajen en equipo.
+                </p>
+                <h4 className='code'>Que hacemos en CodeLaunch:</h4>
             <ul>
-                    <li>Creación de plataformas de gestión y automatización.</li>
-                    <li>Soluciones a medida para pequeñas empresas y emprendimientos.</li>
-                </ul>
+                <li>Alojamos proyectos reales para desarrolladores en formación.</li>
+                <li>Creamos equipos internacionales de programadores.</li>
+                <li>Promovemos la colaboración, las buenas prácticas y el crecimiento profesional.</li>
+            </ul>
+            <h4 className='code'>Impacto obtenido:</h4>
+             <ul>
+                <li>Crecimiento constante de la comunidad.</li>
+                <li>Proyectos completados por equipos globales.</li>
+                <li>Primeros pasos laborales para nuevos desarrolladores.</li>
+            </ul>
             </div>
             </div>
         </div>
